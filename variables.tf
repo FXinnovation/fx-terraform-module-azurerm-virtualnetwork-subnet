f82e7c813092c6d15db3a53570f575c8fc7ed30f
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-21 14:58:18 -0500

    Added variables names to fit main

--- variables.tf
+++ variables.tf
@@ -1,5 +1,14 @@
+variable "resource_group_name" {
+  description = "Name of the resource group where the vnet is hosted."
+  type        = string
+}
 
-variable "subnets" {
+variable "virtual_network_name" {
+  description = "Name of the virtual network where the subnets will reside in."
+  type        = string
+}
+
+variable "subnetsCfg" {
   description = "Object containing deployment information for subnets."
   type        = any
   default = {
--- versions.tf
+++ versions.tf
@@ -1,7 +1,3 @@
 terraform {
   required_version = ">= 0.12"
 }
-
-azurerm {
-  required_version = ">= 1.36.0"
-}
