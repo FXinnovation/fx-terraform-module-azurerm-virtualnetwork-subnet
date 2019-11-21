6abde70c4c3a966b84843ae9f4cb41544d79c951
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-21 14:26:05 -0500

    Modified subnet to do the association though other modules

--- main.tf
+++ main.tf
@@ -1,11 +1,18 @@
+data "azurerm_resource_group" "netrg" {
+  name = var.resource_group_name
+}
 
-resource "azurerm_subnet" "this_subnets" {
-  depends_on                = [azurerm_virtual_network.this]
-  for_each                  = var.subnets
-  name                      = each.value["name"]
-  resource_group_name       = data.azurerm_resource_group.network.name
-  address_prefix            = each.value["address_prefix"]
-  service_endpoints         = lookup(each.value, "service_endpoints", null)
-  route_table_id            = lookup(each.value, "rt_key", null) == null ? null : lookup(azurerm_route_table.rts, each.value["rt_key"], null)["id"]
-  network_security_group_id = lookup(each.value, "nsg_key", null) == null ? null : lookup(azurerm_network_security_group.nsgs, each.value["nsg_key"], null)["id"]
+resource "azurerm_subnet" "this" {
+  for_each             = var.subnets
+  resource_group_name  = data.azurerm_resource_group.netrg.name
+  virtual_network_name = var.virtual_network_name
+  name                 = each.value["name"]
+  address_prefix       = each.value["address_prefix"]
+  service_endpoints    = lookup(each.value, "service_endpoints", null)
+
+  // Avoid collision with subnet_association and route_table_association
+  // Can be removed when azurerm v2.0 will be released
+  lifecycle {
+    ignore_changes = [route_table_id, network_security_group_id]
+  }
 }
