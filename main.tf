ac954fa5a73753ce6dab0df67ca0c09a90e746d7
Maxime Roy <maxime.roy@fxinnovation.com>
2019-11-22 09:56:54 -0500

    fixed variable name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_subnet" "this" {
-  for_each             = var.subnets
+  for_each             = var.subnetsCfg
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
   name                 = each.value["name"]
